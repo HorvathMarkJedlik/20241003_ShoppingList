c420965af4cafd36c460c5491a9b861a6e3f293e
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-10-03 09:33:09 +0200

    gomb count

--- src/app/article-item/article-item.component.ts
+++ src/app/article-item/article-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ArticleModel } from '../models/article.model';
 
 @Component({
@@ -6,6 +6,24 @@ import { ArticleModel } from '../models/article.model';
   templateUrl: './article-item.component.html',
   styleUrl: './article-item.component.css'
 })
-export class ArticleItemComponent {
+export class ArticleItemComponent implements OnInit {
   @Input() article!: ArticleModel;
+
+  constructor(){
+  }
+  ngOnInit(): void {
+      this.article.count = 1;
+
+  }
+
+  increase(){
+    this.article.count++
+  }
+
+  decrease(){
+    if (this.article.count >= 1){
+      this.article.count--;
+    }
+
+  }
 }
