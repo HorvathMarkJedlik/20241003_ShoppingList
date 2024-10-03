a0d6ff2b884acec65896a76af378a5e2e85776ff
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-10-03 08:44:39 +0200

    bevasarlo lista feltoltve

---
+++ src/app/article-item/article-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-article-item',
+  templateUrl: './article-item.component.html',
+  styleUrl: './article-item.component.css'
+})
+export class ArticleItemComponent {
+
+}
